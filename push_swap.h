5d64613556b0a59e281204ba7408b006a9d99d82
Baran Ekrem Serbetcioglu <baserbet@1-C-20.42heilbronn.de>
2026-05-29 18:59:25 +0200

    initial commit

---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baserbet <baserbet@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/29 17:49:46 by baserbet          #+#    #+#             */
+/*   Updated: 2026/05/29 18:29:05 by baserbet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int		size;
+	t_list	*head;
+}	t_stack;
+
+#endif
