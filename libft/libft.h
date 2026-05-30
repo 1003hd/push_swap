5c9c4dec132b67fb5e22032c199aebf810bfb7f7
Baran Ekrem Serbetcioglu <baserbet@1-D-19.42heilbronn.de>
2026-05-30 20:39:39 +0200

    fixed errors

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: baserbet <baserbet@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 16:43:13 by baserbet          #+#    #+#             */
-/*   Updated: 2026/05/30 20:09:07 by baserbet         ###   ########.fr       */
+/*   Updated: 2026/05/30 20:39:15 by baserbet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-long	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
