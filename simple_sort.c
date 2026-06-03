52598ea9ed22960822a35e79af1a0f14b5d2e7b3
selezenart <artvs14@gmail.com>
2026-06-03 17:58:47 +0200

    selection sort adaptation added UNTESTED

---
+++ simple_sort.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aselezen <aselezen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 16:54:25 by aselezen          #+#    #+#             */
+/*   Updated: 2026/06/03 17:24:25 by aselezen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "push_swap.h"
+
+static int	find_max_pos(t_stack *stack)
+{
+	t_list	*curr;
+	int		max;
+	int		max_pos;
+	int		i;
+
+	curr = stack->head;
+	max = curr->content;
+	max_pos = 0;
+	i = 0;
+	while (curr)
+	{
+		if (curr->content > max)
+		{
+			max = curr->content;
+			max_pos = i;
+		}
+		curr = curr->next;
+		i++;
+	}
+	return (max_pos);
+}
+
+static void	move_max_to_top_b(t_stack **b)
+{
+	int	pos;
+	int	size;
+
+	pos = find_max_pos(*b);
+	size = (*b)->size;
+	if (pos <= size / 2)
+	{
+		while (pos-- > 0)
+			rb(b);
+	}
+	else
+	{
+		pos = size - pos;
+		while (pos-- > 0)
+			rrb(b);
+	}
+}
+
+void	selection_sort_stacks(t_stack **a, t_stack **b)
+{
+	while ((*a)->size > 0)
+		pb(a, b);
+
+	while ((*b)->size > 0)
+	{
+		move_max_to_top_b(b);
+		pa(a, b);
+	}
+}
